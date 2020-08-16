715dd9511dd4eacfee97f6faf22233b5bb0ed803
Puneet Mishra <puneet@grexit.com>
2020-08-16 08:48:20 +0000

    tracking code installed

--- src/apis/gettweet.js
+++ src/apis/gettweet.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
 
 export default Axios.create({
-    baseURL: "https://4ssliwb0ad.execute-api.us-east-2.amazonaws.com/prod"
+    baseURL: "https://api.gettweet.in"
 });
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
@@ -27,6 +27,7 @@ const useStyles = makeStyles((theme) => ({
       width: '15ch',
     },
   }));
+const params = new URLSearchParams(window.location.search);
 
 const Search = () => {
     const classes = useStyles();
@@ -41,6 +42,23 @@ const Search = () => {
     const showDownloadComponent = !spinner;
 
     const validateUrl = val => { return /^(?:(?:(?:https?):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(val);}
+
+    const getTweetData = tweet_data => {
+      try {
+        const preloaded_tweet_data = JSON.parse(atob(tweet_data));
+        if (JSON.stringify(preloaded_tweet_data) !== JSON.stringify(tweetData)) {
+          setTweetData(preloaded_tweet_data);
+        }
+      } catch(err) {
+        setTweetData({
+          error: '[FATAL] Server Error, this incidents has been reported. Apologies for the inconvenience.'
+        });
+      }
+    }
+
+    if (params.has('tweet_data')) {
+      getTweetData(params.get('tweet_data'));
+    }
 
     const searchTweet = async status_url => {
       try {
